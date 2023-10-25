06064da81053314cdb32485c49b71683250ed5d6
rickyurvinaUCN <ricardo.urvina@alumnos.ucn.cl>
2023-10-25 09:05:10 -0300

    DeleteUser and postman json

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    //
+    public function getUsers(){
+        return response()->json([
+            'success' => true,
+            'message' => 'Listado de usuarios',
+            'data' => User::all()
+        ], 200);
+    }
+
+    public function store(Request $request){
+        try{
+            DB::beginTransaction();
+            $data= $this->validate($request,[
+                'name' => 'required',
+                'email' => 'required|unique:users',
+                'password' => 'required',
+            ]);
+            User::create($data);
+            DB::commit();
+
+            return response()->json([
+                'message'=>'Usuario creado correctamente',
+                'success'=>True
+            ]);
+
+        }catch (\Throwable $e){
+            DB::rollback();
+            throw  new \Exception($e->getMessage());
+        }
+    }
+
+    public function update(Request $request, User $user){
+        try{
+            DB::beginTransaction();
+
+            $data= $this->validate($request,[
+                'name' => 'required',
+                'email' => 'required',
+                'password' => 'required',
+            ]);
+            if (!$user){
+                return response()->json([
+                    'message'=>'Usuario no encontrado',
+                ],404);
+            }
+            $user->update($data);
+
+            DB::commit();
+
+            return response()->json([
+                'message'=>'Usuario actualizado correctamente',
+                'success'=>True
+            ]);
+
+        }catch (\Throwable $e){
+            DB::rollback();
+            throw  new \Exception($e->getMessage());
+        }
+    }
+
+    public function destroy(User $user){
+        try{
+            DB::beginTransaction();
+            if (!$user){
+                return response()->json([
+                    'message'=>'Usuario no encontrado',
+                ],404);
+            }
+            $user->delete();
+            DB::commit();
+
+            return response()->json([
+                'message'=>'Usuario eliminado correctamente',
+                'success'=>True
+            ]);
+
+        }catch (\Throwable $e){
+            DB::rollback();
+            throw  new \Exception($e->getMessage());
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('users',[UserController::class,'getUsers']);
 Route::post('users',[UserController::class,'store']);
+Route::put('users/{user}',[UserController::class,'update']);
+Route::delete('users/{user}',[UserController::class,'destroy']);
